7acaba2064889be1843ecc0f658119e143feeb79
Abhishek kumar <89406337+abhishekrai17@users.noreply.github.com>
2023-08-29 10:26:09 +0530

    uploaded tut68

---
+++ index.js
@@ -0,0 +1,4 @@
+const average = require("./mod");//const mod
+console.log(average([3,4]));//mod.avg
+console.log("this is index");   
+
---
+++ mod.js
@@ -0,0 +1,17 @@
+console.log("this is mod")
+function average(arr)
+{
+    let sum=0;
+    arr.forEach(element => {
+        sum+=element;
+    });
+    
+    return sum/arr.length;
+}
+
+module.exports = average;
+// module.exports = {
+//     avg:average,
+//     name:"abhi",
+//     repo:"github"
+// }
